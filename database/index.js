f7cfb83a215042bf8f2d0a781db87d74f8e34383
Raziur306 <raziur.rahaman.ronju@gmail.com>
2023-08-03 01:25:24 +0600

    route configuration

--- database/index.js
+++ database/index.js
@@ -1,4 +1,21 @@
 const express = require('express');
-
-
 const app = require('./config/express');
+
+const userRoute = require('./routes/user.routes');
+const adminRoute = require('./routes/admin.routes');
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/user', userRoute);
+app.use('/api/admin', adminRoute);
+
+
+app.get('/', (req, res) => {
+    res.status(200).json({ response: true, message: "The server is running." })
+});
+
+app.use('*', (req, res) => {
+    res.status(404).json({ response: false, message: "404! Bad Request" });
+});
