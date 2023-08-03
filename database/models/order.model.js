c7bb9a0551b62d0fa9ce8492583ac1e5be71176b
Raziur306 <raziur.rahaman.ronju@gmail.com>
2023-08-03 12:41:04 +0600

    order model added

---
+++ database/models/order.model.js
@@ -0,0 +1,20 @@
+const schemaOrder = mongoose.Schema({
+    user_id: mongoose.Schema.Types.ObjectId,
+    order_id: Number,
+    products: [
+        {
+            product_id: mongoose.Schema.Types.ObjectId,
+            name: String,
+            price: Number,
+            quantity: Number,
+        }
+    ],
+    orderTotal: Number,
+    shippingAddress: String,
+    paymentMethod: String
+});
+
+const Order = mongoose.model('orders', schemaOrder);
+
+
+module.exports = Order;
